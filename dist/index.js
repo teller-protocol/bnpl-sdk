28bcf755dee820c5b2935a08db3a6c6ee5bdc082
apex mfer <apexmfer@gmail.com>
2022-06-13 12:32:01 -0400

    exporting method

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.buildExecuteParams = void 0;
+const bnpl_helper_1 = require("./lib/bnpl-helper");
+Object.defineProperty(exports, "buildExecuteParams", { enumerable: true, get: function () { return bnpl_helper_1.buildExecuteParams; } });
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
 
 
 import { buildExecuteParams } from "./lib/bnpl-helper";
+
+
+
+export {buildExecuteParams}

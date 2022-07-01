443bd9eb36179a7e770686f697190328f7972088
Admazzola <admazzola@gmail.com>
2022-07-01 14:08:37 -0400

    can run a test

---
+++ test/spec/bnplhelper.spec.ts
@@ -0,0 +1,22 @@
+
+import chai, { expect } from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+import { BigNumber, Wallet } from 'ethers'
+
+
+chai.should()
+chai.use(chaiAsPromised)
+
+
+
+
+describe('BNPL Helper', () => {
+
+    it('should run a test', async () => {
+
+        expect(1).to.exist
+
+    })
+
+
+})
